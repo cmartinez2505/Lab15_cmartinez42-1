16cb56bbbc4bd6e91fb83700fb33e879a1163c78
Chris <chrismtz2003@gmail.com>
2026-07-28 23:01:12 -0400

    Added my lists

--- Lab15_cmartinez42-1.py
+++ Lab15_cmartinez42-1.py
@@ -6,4 +6,12 @@ Starter Code: None
 Date: July 28, 2026
 """
 
+#My libaries used for the lab
+import math
+import matplotlib.pyplot as plt
+
+
+#Created my empty lists for coordinates
+x = []
+y = []
 
